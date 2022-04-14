58b47c904cec8e5e5586072ed68099358a090923
pzaidins2 <paulzaidins2@gmail.com>
2022-04-14 13:08:46 -0400

    apex import change

--- main.py
+++ main.py
@@ -164,7 +164,7 @@ if __name__ == '__main__':
 
         if args.fp16:
             try:
-                from apex.optimizers import FP16_Optimizer, FusedAdam
+                from apex.contrib.optimizers import FP16_Optimizer, FusedAdam
             except:
                 raise ImportError(
                     "Please install apex from https://www.github.com/nvidia/apex to use distributed and fp16 training.")
